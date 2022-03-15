2a9a2c6bd95943e570429eb4bd036d239e283753
yangyb <1781926993@qq.com>
2022-03-15 22:34:56 +0800

    fix error

--- src/routes/note/index.ts
+++ src/routes/note/index.ts
@@ -4,6 +4,7 @@ import { getFolderList } from './getfileList';
 import { fileContentSearch } from './fileSearch';
 import redis from '../../services/redis';
 import Router from '@koa/router';
+import { errorDealWith } from '@/services/errorDealWith';
 const note = new Router();
 const OVERTIME = 1000 * 60 * 5;  // 数据缓存时间
 
@@ -12,8 +13,7 @@ note.get('/getfile', async(ctx, next) => {
 	const { url }: any = ctx.query;
 	const len = url.split('/').length;
 	if (url.split('/')[len - 1] === 'undefined') {
-		ctx.state.code = 500;
-		ctx.state.msg = "param 'url' is empty";
+		errorDealWith(ctx, 500, '没有找到文件');
 		next();
 	} else {
 		let text = '';
@@ -23,8 +23,7 @@ note.get('/getfile', async(ctx, next) => {
 				return await fs.promises.readFile(src, 'utf-8');
 			}, OVERTIME)
 		} catch {
-			ctx.state.code = 406;
-			ctx.state.msg = "param 'url' corresponding file not found";
+			errorDealWith(ctx, 406, '没有找到文件');
 		}
 		ctx.body = text;
 		next();
