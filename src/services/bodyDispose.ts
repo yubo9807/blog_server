ae023af78ac85508e798c0980b65e1f442ccac1d
yangyb <yangyb@boyachain.cn>
2022-05-29 08:52:57 +0800

    add blacklist

--- src/services/bodyDispose.ts
+++ src/services/bodyDispose.ts
@@ -2,7 +2,7 @@ import env from '../env';
 import { Context } from 'koa';
 import { errorDealWith } from './errorDealWith';
 import preventDataRefresh from './preventDataRefresh';
-import { printErrorLogs } from './logger';
+import { sql_addBlockList, sql_queryBlockList } from '@/spider/blacklist';
 
 export default async(ctx: Context, next: () => {}) => {
 
@@ -17,16 +17,23 @@ export default async(ctx: Context, next: () => {}) => {
 	ctx.state.redis_cache = false;
 
 	// 数据防刷
+	const queryBlackList = await sql_queryBlockList(ctx.request.ip);
+	if (queryBlackList[0]) {
+		errorDealWith(ctx, 500, '您已被限制访问，请联系管理员', false);
+	}
 	const beyondRefresh = await preventDataRefresh(ctx);
 	if (beyondRefresh) {
-		errorDealWith(ctx, 513);
+		const { request_rate } = ctx.state;
+		if (request_rate > 100) {  // 请求频率超过100，加入黑名单
+			await sql_addBlockList(ctx.request.ip, request_rate);
+		}
+		errorDealWith(ctx, 513, null, false);
 	}
 
 	// 打印日志
 	try {
 		await next();
 	} catch (error) {
-		printErrorLogs(ctx, error);
 		// code 依然为 400，说明错误并没有被捕获到
 		if (ctx.state.code === 400 && error) {
 			errorDealWith(ctx, 400);
@@ -38,7 +45,7 @@ export default async(ctx: Context, next: () => {}) => {
 	const endTime = Date.now();
 
 	if (state.msg == '' && !body) {  // 没错误消息，也没body
-		errorDealWith(ctx, 404);
+		errorDealWith(ctx, 404, null, false);
 	} else if (body) {  // 正常返回数据
 		ctx.body = {
 			code: 200,
---
+++ src/spider/blacklist.ts
@@ -0,0 +1,40 @@
+import { getNowDate } from '@/utils/date';
+import connection from './connection';
+
+/**
+ * 加入到黑名单
+ * @param ip ip地址
+ * @param requestNumber 请求次数
+ * @returns 
+ */
+export async function sql_addBlockList(ip: string, requestNumber: number) {
+  const createTime = getNowDate();
+  const connect = await connection();
+  const [ rows ] = await connect.execute(`INSERT INTO blacklist (ip, create_time, request_number) VALUES(?, ?, ?);`, [ ip, createTime, requestNumber ]);
+  connect.end();
+  return rows;
+}
+
+/**
+ * 查找黑名单列表
+ * @param ip 查询ip
+ * @returns 
+ */
+export async function sql_queryBlockList(ip: string | null = null) {
+  const connect = await connection();
+  const [ rows ] = await connect.execute(ip ? `SELECT * FROM blacklist WHERE ip = '${ip}';` : `SELECT * FROM blacklist;`);
+  connect.end();
+  return rows;
+}
+
+/**
+ * 删除黑名单列表ip
+ * @param ip 查询ip
+ * @returns 
+ */
+export async function sql_deleteBlockList(ip: string) {
+  const connect = await connection();
+  const [ rows ] = await connect.execute(`DELETE FROM blacklist WHERE ip = '${ip}';`);
+  connect.end();
+  return rows;
+}
