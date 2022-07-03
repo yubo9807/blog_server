6789124f6415c0e5a3231777df5cdc8633d38444
yangyb <yangyb@boyachain.cn>
2022-07-03 17:45:13 +0800

    fix os

--- src/routes/os/index.ts
+++ src/routes/os/index.ts
@@ -6,20 +6,31 @@ const route = new Router();
 // 获取系统信息
 route.get('/', async(ctx, next) => {
   
-  const { arch, cpus, freemem, totalmem, hostname, endianness, loadavg, release, type, uptime, version } = os;
+  const { arch, cpus, hostname, endianness, release, type, uptime, version } = os;
 
   ctx.body = {
     arch: arch(),
-    cpu: cpus()[0],  // cpu 信息 *
-    freemem: freemem(),  // 可用内存 *
-    totalmem: totalmem(),  // 总内存
+    cpu: cpus()[0],  // cpu 信息
     hostname: hostname(),  // 主机名
     endianness: endianness(),  // 大端序 'BE' || 小端序 'LE'
-    loadavg: loadavg(),  // 负载
     release: release(),  // 操作系统
     type: type(),  // 操作系统类型
-    uptime: uptime(),  // 系统运行时间(秒) *
+    uptime: uptime(),  // 系统运行时间(秒)
     version: version(),  // 内核版本
+  };
+  next();
+})
+
+
+// 动态变化的数据
+route.get('/dynamic', async(ctx, next) => {
+
+  const { freemem, totalmem, loadavg } = os;
+
+  ctx.body = {
+    freemem: freemem(),  // 可用内存
+    totalmem: totalmem(),  // 总内存
+    loadavg: loadavg(),  // 负载
   };
   next();
 })
