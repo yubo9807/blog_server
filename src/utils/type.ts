9e44012789f62dac48efeb75f6cb96f219e25ccf
yangyb <yangyb@boyachain.cn>
2022-10-25 10:22:57 +0800

    feat: 装饰器类型校验

---
+++ src/utils/type.ts
@@ -0,0 +1,16 @@
+
+// 任意类型对象
+export type AnyObj = {
+  [prop: string | number | symbol]: any
+}
+
+export type Type = 'string'    | 'number' | 'boolean' |
+                   'symbol'    | 'bigint' |
+                   'undefined' | 'null'   |
+                   'array'     | 'object' |
+                   'function'  |
+                   'set'       | 'map'
+
+export function isType(o: any): Type {
+  return Object.prototype.toString.call(o).slice(8, -1).toLowerCase();
+}
